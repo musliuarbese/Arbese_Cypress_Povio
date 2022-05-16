4d75af9e10dc9e57d7492bc4b9ef2d4ff1cfab3b
Arbesë Musliu <63508538+musliuarbese@users.noreply.github.com>
2022-05-16 15:30:15 +0200

    Automated the third requirement: that checks that you can’t register with an already existing email

--- cypress/integration/StepsDefinitions/SignUpStepsDefinitions.js
+++ cypress/integration/StepsDefinitions/SignUpStepsDefinitions.js
@@ -89,6 +89,39 @@ describe('Test Sign Up', () => {
 
     })
 
+    it(['SmokeTests', 'Tags'], 'Try to register a new user with an already existing email', () => {
+
+        signUp.isAtPovioPage().
+        should('have.text', 'Welcome')
+
+        signUp.clickOnSignUp().
+        should('have.text', 'Sign up')
+
+        signUp.clickOnSignUp().click()
+
+        signUp.isAtSignUpPage()
+            .should('be.visible')
+
+        signUp.userNameInput().type(this.data.userName)
+            .should('have.value', this.data.userName)
+
+        signUp.emailInput().type(this.data.email)
+            .should('have.value', this.data.email)
+
+        signUp.passwordInput().type(this.data.password)
+            .should('have.value', this.data.password)
+
+        signUp.passwordConfirmationInput().type(this.data.passwordConfirm)
+            .should('have.value', this.data.passwordConfirm)
+
+        signUp.signUpButton().click()
+
+        signUp.emailAlreadyTaken()
+            .should('have.text', 'Email has already been taken')
+
+    })
+
+
 })
 
 
